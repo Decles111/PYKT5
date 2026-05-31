7b2bdf54364045f009b8ed98f9bad0ee772f2f3c
Decles111 <yakshinem24@st.ithub.ru>
2026-05-31 20:37:44 +0300

    Update main.py

--- PyKT5/main.py
+++ PyKT5/main.py
@@ -1,41 +1,62 @@
 from tkinter import Tk, Label
 from datetime import datetime
 
+
+def read_file():
+    with open("tasks.txt", "r", encoding="utf-8") as file:
+        return file.readlines()
+
+
+def color(days):
+    if days < 0:
+        return "red"
+    if days == 0:
+        return "yellow"
+    return "white"
+
+
+def make_list(tasks):
+    old = []
+    now = []
+    fu = []
+    k = 0
+
+    td = datetime.now().date()
+
+    for line in tasks:
+        k += 1
+
+        plan, date = line.strip().split(";")
+        date = datetime.strptime(date, "%Y-%m-%d").date()
+
+        days = (date - td).days
+
+        if days < 0:
+            old.append([f"Прошло {-days} дней от {plan}", days])
+        elif days == 0:
+            now.append([f"Прямо сейчас происходит {plan}", days])
+        else:
+            fu.append([f"Осталось {days} дней до {plan}", days])
+
+    old = sorted(old, key=lambda x: x[1])
+    fu = sorted(fu, key=lambda x: x[1])
+
+    return old + now + fu, k
+
+
 root = Tk()
 root.title("Что мне делать, как мне жить?")
-root.geometry("800x500")
 root.configure(bg="black")
 
-Label(root, text="Мои текущие задачи", fg="yellow", bg="black", font=("IMPACT", 34, "underline")).pack(pady=20)
+Label(root,text="Мои текущие задачи",fg="yellow",bg="black",font=("IMPACT", 34, "underline")).pack(pady=20)
 
-today = datetime.now().date()
+tasks = read_file()
 
+data, k = make_list(tasks)
 
+root.geometry(f"1000x{200 + k * 25}")
 
-with open("tasks.txt", "r", encoding="utf-8") as file:
-    tasks = file.readlines()
-
-
-
-for line in tasks:
-    plans, ds = line.strip().split(";")
-
-    td = datetime.strptime(ds, "%Y-%m-%d").date()
-
-    kogda = (td - today).days
-
-    if kogda < 0:
-        text = f"Прошло {-kogda} дней от {plans}"
-        color = "red"
-
-    elif kogda == 0:
-        text = f"Прямо сейчас происходит {plans}"
-        color = "yellow"
-
-    else:
-        text = f"Осталось {kogda} дней до {plans}"
-        color = "white"
-
-    Label(root, text=text, fg=color, bg="black", anchor="w", font=("Arial", 14)).pack(fill="x", padx=150)
+for text, days in data:
+    Label(root,text=text,fg=color(days),bg="black",anchor="w",font=("Arial", 14)).pack(fill="x", padx=30)
 
 root.mainloop()
